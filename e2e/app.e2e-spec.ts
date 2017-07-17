ee0ae17e42970575ebe49f9cb374f0b7f0637166
Gary Carter <garyscarter@gmail.com>
2017-07-17 14:12:12 +0100

    Initial Framework Setup

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -7,8 +7,4 @@ describe('playing-card-app App', () => {
     page = new PlayingCardAppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
-  });
 });
